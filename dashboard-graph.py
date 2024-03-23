0ddd7dc08c8b528f8ebcc2f0b31ac4c7f234229f
Phanurat <paunavt5@gmail.com>
2024-03-23 13:20:47 +0700

    Add Folder Json file

--- dashboard-graph.py
+++ dashboard-graph.py
@@ -1,3 +1,4 @@
+import json
 from binance.client import Client
 
 client = Client()
@@ -8,3 +9,16 @@ bnb_usdt = client.get_symbol_ticker(symbol='BNBUSDT')
 print(btc_usdt,"USDT")
 print(eth_usdt,"USDT")
 print(bnb_usdt,"USDT")
+
+# สร้าง dictionary เก็บข้อมูล
+data = {
+    "BTCUSDT": btc_usdt,
+    "ETHUSDT": eth_usdt,
+    "BNBUSDT": bnb_usdt
+}
+
+# บันทึกข้อมูลในไฟล์ JSON
+with open('json/prices.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
+
+print("บันทึกข้อมูลเรียบร้อยแล้วในไฟล์ prices.json")
